14ac768c8e21cf63f05c79e238b697a40d770af3
georgeowusu <gkojunior@gmail.com>
2024-01-27 15:07:42 -0500

    create routes for the different pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import About from './components/About'
+import Contact from './components/Contact'
+import Landing from './components/Landing'
+import Services from './components/Services'
+
 function App() {
-	return <>notary avenue</>
+	return (
+		<Router>
+			<Routes>
+				<Route path='/' element={<Landing />} />
+				<Route path='about' element={<About />} />
+        <Route path='services' element={<Services />} />
+        <Route path='contact' element={<Contact />} />
+				<Route path='*' element={<Error />} />
+			</Routes>
+		</Router>
+	)
 }
 
 export default App
